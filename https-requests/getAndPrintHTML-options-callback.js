72d844e2eeffd7ee19ff0c74cadd9f0a8aca6dc2
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-31 15:09:31 +0000

    Adding getAndPrintHTML function with options and a callback function

---
+++ https-requests/getAndPrintHTML-options-callback.js
@@ -0,0 +1,34 @@
+function getAndPrintHTML (options) {
+
+  var https = require('https');
+
+  var html = ''
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8')
+
+    response.on('data',function(data) {
+      html += data
+    })
+
+    response.on('end',function() {
+      printHTML(html)
+    })
+
+  });
+
+
+}
+
+
+function printHTML (html) {
+  console.log(html);
+}
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+getAndPrintHTML(requestOptions)
