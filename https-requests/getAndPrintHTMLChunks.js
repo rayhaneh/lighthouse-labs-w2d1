a8ef3b8718c2e23f1ecf250f517c769ff3d94252
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-31 14:41:28 +0000

    Adding getAndPrintHTMLChunks function

---
+++ https-requests/getAndPrintHTMLChunks.js
@@ -0,0 +1,28 @@
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8')
+
+    response.on('data',function(data) {
+      console.log(data + '\n')
+    })
+
+    response.on('end',function() {
+      console.log('Response stream complete.')
+    })
+
+  });
+
+
+}
+
+getAndPrintHTMLChunks()
